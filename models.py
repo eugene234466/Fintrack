d332cdbbabf07ff299b636a06a4a6aad19b2518c
Eugene Yarney <eugeneyarney5@gmail.com>
2026-07-04 14:23:30 +0000

    Refactor database handling and transaction retrieval
    Refactor database connection and initialization logic, update date filtering for transactions.

--- models.py
+++ models.py
@@ -1,12 +1,10 @@
 import sqlite3
 from datetime import datetime, timedelta
-import tempfile
 import os
 
-DATABASE = os.path.join(tempfile.gettempdir(), "finance.db")
 
-# Ensure directory exists (important!)
-os.makedirs(os.path.dirname(DATABASE), exist_ok=True)
+BASE_DIR = os.pathdirname(os.path.abspath(__file__))
+DATABASE = os.path.join(BASE_DIR, "finance.db")
 
 def get_connection():
     conn = sqlite3.connect(DATABASE)
@@ -14,9 +12,11 @@ def get_connection():
     return conn
 
 def init_db():
-    """Initialize the database with required tables"""
+    """Intialize the database with required tables"""
+    
     with get_connection() as conn:
         cursor = conn.cursor()
+        
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS transactions(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -25,58 +25,62 @@ def init_db():
             category TEXT NOT NULL,
             date DATE NOT NULL,
             note TEXT
-        )
-        ''')
+        )                                  
+    ''')
+        
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS budgets(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             category TEXT UNIQUE NOT NULL,
             monthly_limit REAL NOT NULL
-        )
+        )                                  
         ''')
         conn.commit()
-
+        
 
 def add_transactions(type, amount, category, date, note):
     with get_connection() as conn:
         cursor = conn.cursor()
+        
         cursor.execute(
             "INSERT INTO transactions(type, amount, category, date, note) VALUES(?,?,?,?,?)",
             (type, amount, category, date, note)
         )
         conn.commit()
-
-
+        
+        
 def get_transactions(period="weekly"):
     today = datetime.now().date()
-
+    print(today)        
+    
     if period == "weekly":
-        days_since_monday = today.weekday()
-        date_filter = today - timedelta(days=days_since_monday)
+        date_filter = today - timedelta(days = 7)
+        print(f"Fetching weekly transaction since {date_filter}")
     else:
-        date_filter = today.replace(day=1)
-
+        date_filter = (today - timedelta(days=180)).replace(day=1) 
+        print(f"Fetching transaction data since {date_filter}")
+        
     with get_connection() as conn:
         cursor = conn.cursor()
         cursor.execute("""
           SELECT * FROM transactions
           WHERE date >= ?
-          ORDER BY date DESC
-        """, (date_filter,))
-        transactions = cursor.fetchall()
+          ORDER BY date DESC                            
+        """,(date_filter,))
+        
+        
+        transactions = cursor.fetchall() 
     return transactions
-
 
 def delete_transaction(id):
     with get_connection() as conn:
         cursor = conn.cursor()
         cursor.execute(
             "DELETE FROM transactions WHERE id = ?",
-            (id,)
+            (id,)  
         )
         conn.commit()
-
-
+            
 def set_budget(category, monthly_limit):
     with get_connection() as conn:
         cursor = conn.cursor()
@@ -85,11 +89,14 @@ def set_budget(category, monthly_limit):
             (category, monthly_limit)
         )
         conn.commit()
-
-
+        
+        
 def get_budget():
     with get_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM budgets")
+        cursor.execute(
+            "SELECT * FROM budgets"
+        )
         budgets = cursor.fetchall()
         return budgets
+    
